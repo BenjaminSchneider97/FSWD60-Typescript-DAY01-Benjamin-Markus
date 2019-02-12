41d5106ed733fcd44b2ca3459b2c0eab4c35672c
Markus Szokoll <markus.szokoll@gmx.at>
2019-02-12 09:57:58 +0100

    basic exercise

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (var i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length; j++) {
-        document.write(array[i] + " X " + array[j] + " = " + (array[i] * array[j]) + "<br>");
+array.forEach(function (value) {
+    array.forEach(function (value2) {
+        document.write(value + " X " + value2 + " = " + (value * value2) + "<br>");
+    });
+});
+for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
+    var value = array_1[_i];
+    for (var _a = 0, array_2 = array; _a < array_2.length; _a++) {
+        var value2 = array_2[_a];
+        document.write(value + " X " + value2 + " = " + (value * value2) + "<br>");
     }
 }
--- script.ts
+++ script.ts
@@ -1,9 +1,16 @@
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+array.forEach(function(value){
+	array.forEach(function(value2){
+		document.write(value + " X " + value2 + " = " + (value*value2) + "<br>");
+	});
+});
 
 
-for (let i = 0; i < array.length; i++) {
-	for (let j = 0; j < array.length; j++){
-		document.write(array[i] + " X " + array[j] + " = " + (array[i]*array[j]) + "<br>");
-	}
-   
+
+
+for (var value of array) {
+   for (var value2 of array) {
+   		document.write(value + " X " + value2 + " = " + (value*value2) + "<br>");
+
+}
 }
